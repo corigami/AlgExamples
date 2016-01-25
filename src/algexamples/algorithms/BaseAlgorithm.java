75ae46065901e3929f65bf5cf50b6e8c070ddd3d
Corey Willinger <corey.willinger@gmail.com>
2016-01-25 14:45:13 -0500

    Docs: added some javaDocs and cleaned up a bit

--- src/algexamples/algorithms/BaseAlgorithm.java
+++ src/algexamples/algorithms/BaseAlgorithm.java
@@ -1,24 +1,37 @@
-
 package algexamples.algorithms;
 
 /**
  * Basic abstract boilerplate to give Algorithms common parent
+ *
  * @author Corey
  */
-public abstract class BaseAlgorithm implements IAlgorithm{
+public abstract class BaseAlgorithm implements IAlgorithm {
+
     public BaseAlgorithm() {
-        }
-    
+    }
+
+    /**
+     * Main function to initiate algorithm example.
+     *
+     * @return String
+     */
     @Override
     public abstract int runAlgorithm();
 
+    /**
+     * Returns the title of the Algorithm
+     *
+     * @return String
+     */
     @Override
     public abstract String getTitle();
-    
+
+    /**
+     * Returns the String sequence current step of the Algorithm
+     *
+     * @return String
+     */
     @Override
     public abstract String getCurrentStep();
-    
-    
-    
-    
+
 }
